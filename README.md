046cb44382b5e8f912d751b87f228e3a716a2108
Leen Barakat <leenbarakat2432001@outlook.com>
2021-09-21 16:03:08 -0700

    add lab6

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # city-explorer
+
+**Author**: Leen Barakat
+**Version**: 1.0.0 (increment the patch/fix version number if you make more commits past your first submission)
+
+## Overview
+I have built this application to find different places and know where they are located at 
+
+## Getting Started
+should consider knowing how to render and make constructor 
+## Architecture
+
+I have used github to create reposotories and useed the ubuntu terminal to connect the vs code withe the github 
+## Change Log
+<!-- Use this area to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an example:
+
+01-01-2001 4:59pm - Application now has a fully-functional express server, with a GET route for the location resource. -->
+
+## Credit and Collaborations
+I have used w3schools and watched the recorded lectures by my instrocture Razan and my TA Boshra helped me a lot tbh
--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      imgUrl: '',
+      locationResult: {},
+      searchQuery: ''
+    }
+    
+  }
+
+  getLocFun = async (e) => {
+    e.preventDefault();
+    console.log('inside getLocFun')
+
+    await this.setState({
+      searchQuery: e.target.city.value
+    })
+
+    console.log('key',process.env.REACT_APP_LOCATIONIQ_KEY);
+
+
+    let reqUrl = ` https://eu1.locationiq.com/v1/search.php?key=pk.de44a9504016551ea3a0451181157fb2&q=${this.state.searchQuery}&format=json`
+    
+    let locResult = await axios.get(reqUrl);
+    console.log('hey',locResult);
+    console.log('hello', locResult.data);
+    console.log('hi', locResult.data[0]);
+
+    this.setState({
+      locationResult:locResult.data[0]
+    })
+
+    let imgUrl=`https://maps.locationiq.com/v3/staticmap?key=pk.de44a9504016551ea3a0451181157fb2&center=${this.state.locationResult.lat},${this.state.locationResult.lon}&zoom=8`
+    this.setState({imgUrl:imgUrl})
+  
+  }
+  
+  render() {
+    return (
+      <div>
+        <h3>City Explorer app</h3>
+       {/* <button onClick={this.getLocFun}>Get Location</button >*/}
+        <form onSubmit={this.getLocFun}>
+          <input type = "text" name = 'city'/>
+          <input type = "submit" value = 'click to find a city'/>
+          </form>
+        
+        <p>City Name: {this.state.searchQuery}</p>
+        <p>latitude: {this.state.locationResult.lat} </p>
+        <p>longtitude: {this.state.locationResult.lon} </p>
+
+
+
+        <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src={this.state.imgUrl} />
+  <Card.Body>
+    <Card.Title>{this.state.searchQuery}</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+      </div>
+
+
+    )
+  }
 }
-
 export default App;
